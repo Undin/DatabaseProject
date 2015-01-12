c2992c72746fec18b4b12a265dbc8e401982f3b0
Undin <arseniy1993@mail.ru>
2015-01-12 04:48:34 +0300

    run all sql files in right order

---
+++ recreate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sqlDir="/Users/warrior/Programming/DatabaseProject/sql"
+psql HSDatabase -f "$sqlDir/removal.sql"
+psql HSDatabase -f "$sqlDir/creation.sql"
+psql HSDatabase -f "$sqlDir/cards.sql"
+psql HSDatabase -f "$sqlDir/effects.sql"
+psql HSDatabase -f "$sqlDir/players.sql"
+psql HSDatabase -f "$sqlDir/heroes.sql"
+psql HSDatabase -f "$sqlDir/has_effect.sql"
+psql HSDatabase -f "$sqlDir/has_card.sql"
+psql HSDatabase -f "$sqlDir/decks.sql"
+psql HSDatabase -f "$sqlDir/in_deck.sql"
+
+
